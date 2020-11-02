0b51432bf65db5c82b12b7901df748a8f3b9bda6
Ahasan Habib <habibsumoncse@gmail.com>
2020-11-02 11:28:29 +0600

    add configure unit testing code

--- spring-boot-rest-curd/src/test/java/com/spring/rest/ApplicationTester.java
+++ spring-boot-rest-curd/src/test/java/com/spring/rest/ApplicationTester.java
@@ -1,25 +1,33 @@
 package com.spring.rest;
 
+import org.hibernate.query.criteria.internal.expression.SearchedCaseExpression.WhenClause;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.spring.rest.controller.EmployeeController;
+import com.spring.rest.dto.EmployeeDTO;
 import com.spring.rest.service.EmployeeService;
 
 @ExtendWith(MockitoExtension.class)
 class ApplicationTester {
-	
+
 	@InjectMocks
 	EmployeeController employeeController;
-	
+
 	@Mock
 	EmployeeService employeeService;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+//		when(employeeService.createOrUpdateEmployee(any(EmployeeDTO.class))).thenReturn(true);
 	}
 
 }
