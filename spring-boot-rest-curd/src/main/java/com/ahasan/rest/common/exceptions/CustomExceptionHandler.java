3eba3abc176643c7e3e6e341f434232e46e31a16
Ahasan Habib <habibsumoncse@gmail.com>
2020-11-24 15:52:05 +0600

    configure exeception handaling on controller class

--- spring-boot-rest-curd/src/main/java/com/ahasan/rest/common/exceptions/CustomExceptionHandler.java
+++ spring-boot-rest-curd/src/main/java/com/ahasan/rest/common/exceptions/CustomExceptionHandler.java
@@ -30,6 +30,14 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
 	}
 
+	@ExceptionHandler(MissingHeaderInfoException.class)
+	public final ResponseEntity<ErrorResponse> handleInvalidTraceIdException(MissingHeaderInfoException ex,
+			WebRequest request) {
+		List<String> details = new ArrayList<>();
+		details.add(ex.getLocalizedMessage());
+		ErrorResponse error = new ErrorResponse(BAD_REQUEST, details);
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
 
 	@ExceptionHandler(ConstraintViolationException.class)
 	public final ResponseEntity<ErrorResponse> handleConstraintViolation(ConstraintViolationException ex,
--- spring-boot-rest-curd/src/main/java/com/ahasan/rest/controller/EmployeeController.java
+++ spring-boot-rest-curd/src/main/java/com/ahasan/rest/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.ahasan.rest.common.messages.BaseResponse;
 import com.ahasan.rest.dto.EmployeeDTO;
 import com.ahasan.rest.service.EmployeeService;
 
+@Validated
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
